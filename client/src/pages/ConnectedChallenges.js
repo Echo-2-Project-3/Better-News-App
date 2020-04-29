18fce91a54b6cd5bdba7427cd937f5b7b6d0a90c
Jeff.A <kinesthesiac@gmail.com>
2020-04-28 23:18:56 -0700

    Added text to the 1st challenge describing the rules. Font color changes.

--- client/src/pages/ConnectedChallenges.js
+++ client/src/pages/ConnectedChallenges.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function ConnectedChallenge() {
     return (
+        <div id="cnctdChal">
         <h1>Connected Challenge Page</h1>
+        <br />
+        <h3>Welcome to the staying connected challegne.  This is a great opportunity to help maintain a community you're involved in durring stay at home.  Reach out to at least one member of your community at least once a day, and post the interaction here.  You must post at least once per day to finish the challenge. One point earned per post.</h3>
+        </div>
     );
 };
 
