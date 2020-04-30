78970bc54028a3007defb607dfdcb252adfc5c22
chelsealuttrall <chelsealuttrall@gmail.com>
2020-04-30 13:20:16 -0600

    landing page with module and re-routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import virus from "./virus.jpg";
+//import virus from "./virus.jpg";
 import axios from "axios";
 import ConnectedChallenge from "./pages/ConnectedChallenges.js";
+import AppWelcome from "./pages/AppWelcome.js";
 import ProfilePage from "./pages/profilepage.js";
 //import ButtonComponent from "./components/ButtonsComponent.js";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
@@ -10,31 +11,16 @@ import UserHome from "./pages/UserHome.js";
 import Nav from "./components/Navbar.js";
 import "./styles/userHome.css";
 import "./App.css";
-import SignIn from "./components/Login.SignIn.js";
+//import SignIn from "./components/Login.SignIn.js";
 
 
 
 class App extends Component {
   state = {
-    avatars: [],
-    userName: ""
+    
   };
   componentDidMount() {
-    // this.getAvatars();
-  }
-
-  getAvatars = () => {
-    axios
-      .get("/api/avatar")
-      .then((res) => {
-        console.log("res", res);
-        this.setState({
-          avatars: res.data.asset_ids,
-        });
-      })
-      .catch((err) => {
-        console.log("the err", err);
-      });
+    
   };
   render() {
 
@@ -44,52 +30,16 @@ class App extends Component {
         <Router>
         <Nav/>
         <Switch>
-          <Route exact path="/" component={UserHome}/>
-
+          <Route exact path="/" component ={AppWelcome}/>
+          <Route exact path="/Home" component={UserHome}/>
           <Route path="/challenges" >
             <ConnectedChallenge userName={this.state.userName} />
           </Route>
-
           <Route exact path="/profilepage" component={ProfilePage}/>
         </Switch>
           
       </Router>
-        <SignIn loggedIn={this.state.show}></SignIn>
-        <SignIn/>
-          <p>Welcome to Viral Growth!</p>
-
-        
-
-          <img src={virus} className="Virus-logo" alt="irus" height="200px" />
-          <p>Please log in to get started!</p>
-          {/* </header> */}
-          {/* <button type="button" className="btn btn-danger">
-            Get Started!
-            </button>
-            <br />
-            <button type="button" className="btn btn-info">
-            Login!
-          </button> */}
-          {/* <Login/> */}
-          <br />
-       
-          <br />
-          {/* {this.state.avatars.map(avatar => {
-            return (
-              <h5 style={{ backgroundColor: "black" }}><img src={avatar.thumbnailSrc} style={{width: '250px', height: '250px'}}/> </h5>
-            )
-          })} */}
-         
-          {/*<p>Hello User!</p>
-          <p>What would you like to do today?</p>
-         
-          
-          <button type="button" className="btn btn-light">
-            
-          </button>*/}
-       
-          {/* <ConnectedChallenge /> */}
-          
+                 
       </div>
 
       
