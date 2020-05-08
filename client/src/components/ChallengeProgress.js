ecf81f478d03d123d919cb64c1e37e7e207328ca
Jeff.A <kinesthesiac@gmail.com>
2020-05-07 17:19:50 -0700

    Just a minor fiddle with style.

--- client/src/components/ChallengeProgress.js
+++ client/src/components/ChallengeProgress.js
@@ -3,20 +3,22 @@ import { Link } from 'react-router-dom';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 // import user from '';
 import data from '../data/ChallengeInfo.json';
+import "../styles/challengeCardStyling.css";
 
 // let points = user.challenges.points//user's accrued points;
-let progressPercent = (points/(data[1].maxpoints)*100);
+let progressPercent = (points / (data[1].maxpoints) * 100);
 
 let points = 1;
 
-function ChallengeProgress () {
-    
-    
-    console.log("progress", progressPercent);
-    
+function ChallengeProgress() {
+
+
+  console.log("progress", progressPercent);
+
   return (
-    <div>
-       <ProgressBar animated variant="success" now={progressPercent.point} />
+    <div class="progressBar1">
+      <ProgressBar animated variant="success" now={progressPercent.point} />
+      <span>{progressPercent.point}% Complete</span>
     </div>
   )
 
