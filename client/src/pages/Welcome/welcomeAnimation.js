ca6e7f856466d7e0c0b144962cd3e3a76fc115f5
chelsealuttrall <chelsealuttrall@gmail.com>
2020-05-16 22:38:16 -0600

    generic avatars next to posts. posts sized.

--- client/src/pages/Welcome/welcomeAnimation.js
+++ client/src/pages/Welcome/welcomeAnimation.js
@@ -1,20 +1,26 @@
 
-import $ from "jquery";
+// import $ from "jquery";
 
-// $(document).mousemove(function(event) {
-//   var eye = $(".eye");
-//   var x = (eye.offset().left) + (eye.width() / 2);
-//   var y = (eye.offset().top) + (eye.height() / 2);
-//   var rad = Math.atan2(event.pageX - x, event.pageY - y);
-//   var rot = (rad * (180 / Math.PI) * -1) + 180;
-//   eye.css({
-//     '-webkit-transform': 'rotate(' + rot + 'deg)',
-//     '-moz-transform': 'rotate(' + rot + 'deg)',
-//     '-ms-transform': 'rotate(' + rot + 'deg)',
-//     'transform': 'rotate(' + rot + 'deg)'
-//   });
+// function handleEyes(event) {
+//     var eye = $(".eye");
+//     console.log("eyeye: ", eye);
+//     var x, y, rad, rot;
+//     let eye_offset = eye.offset();
+//     if (eye_offset) {
+//       x = eye_offset.left + eye.width() / 2;
+//       y = eye_offset.top + eye.height() / 2;
+//       rad = Math.atan2(event.pageX - x, event.pageY - y);
+//       rot = rad * (180 / Math.PI) * -1 + 180;
+//       eye.css({
+//         "-webkit-transform": "rotate(" + rot + "deg)",
+//         "-moz-transform": "rotate(" + rot + "deg)",
+//         "-ms-transform": "rotate(" + rot + "deg)",
+//         transform: "rotate(" + rot + "deg)",
+//       });
+//     }
+//   }
   
-// });
+//   $("#root").on('mousemove', handleEyes);
 
 
 // $("body").mousemove(function(event) {
